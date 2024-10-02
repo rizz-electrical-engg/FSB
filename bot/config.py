5d6a0e07ae860134d031a19f2681cf375257b79b
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-10-02 22:01:14 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,7 +13,7 @@ class Telegram:
 class Server:
     BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8080))
+    PORT = int(env.get("PORT", 9090))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
